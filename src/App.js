f40e1889109f948f8dda49cf0273dbcc338df2f3
dearshrewdwit <dearshrewdwit@gmail.com>
2021-11-03 23:56:47 +0000

    comment out and remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import TodoList from './components/TodoList/TodoList';
+// import TodoList from './components/TodoList/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
     </div>
   );
 }
